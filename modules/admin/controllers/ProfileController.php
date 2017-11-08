260ec00c644aa944e9080e233f727d415d4dbe01
Ivashka-04 <iskatel04@gmail.com>
2017-11-09 00:45:04 +0600

    Добавили Профайл контроллер

---
+++ modules/admin/controllers/ProfileController.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ivashka
+ * Date: 09.11.2017
+ * Time: 0:35
+ */
+
+namespace app\modules\admin\controllers;
+
+
+use yii\filters\AccessControl;
+use yii\web\Controller;
+use Yii;
+
+class ProfileController extends Controller
+{
+    public function behaviors()
+    {
+        return [
+          'access' => [
+              'class' => AccessControl::className(),
+              'rules' => [
+                  [
+                      'allow' => true,
+                      'roles' => '@',
+                  ],
+              ],
+          ],
+        ];
+    }
+
+
+    public function  actionIndex()
+    {
+        return $this->render('index',['model' => $this->findModel(),]);
+    }
+
+    /**
+     * @return User the loaded model
+     */
+
+    private function findModel(){
+        return User::findOne(Yii::$app->user->identity->getId());
+    }
+
+
+}
